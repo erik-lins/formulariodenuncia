ab651ffcbc07a4720a1c261d716f1b9155ff9624
erik-lins <58609919+erik-lins@users.noreply.github.com>
2024-03-15 13:29:27 -0300

    Formulario_denuncia
    Formulario de denuncia

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 function submitReport() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var incidentReport = document.getElementById('incidentReport').value;
-    var isAnonymous = document.getElementById('anonymousCheckbox').checked;
+    // Lógica de envio da denúncia
+    // ...
 
-    if (incidentReport.trim() !== '') {
-        alert('Sua denúncia foi enviada com sucesso. Obrigado por contribuir para a segurança.');
-        // Limpar o formulário após o envio bem-sucedido
-        document.getElementById('incidentForm').reset();
-        // Exibir a mensagem de sucesso
-        document.getElementById('successMessage').innerHTML = 'Sua denúncia foi enviada com sucesso. Obrigado por contribuir para a segurança.';
-        // Adicionar lógica para enviar a denúncia para um servidor ou armazenar localmente
-    } else {
-        alert('Por favor, descreva o incidente antes de enviar.');
-    }
+    // Após o envio, redirecionar para a página de confirmação
+    window.location.href = 'confirmacao.html';
 }
